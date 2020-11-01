c76e6b85572d5a7f72f0bbc2c3105ff45547b4b4
Alexander <aps98@mail.ru>
2020-11-01 17:26:14 +0300

    die

--- data/script.py
+++ data/script.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import uniform, normal
+from scipy.special import erf
 
 
 labels = ["Детерминированный", "Случайная фаза",
           "Случайная амплитуда", "Случайная фаза и амплитуда"]
 excel_files = ["data/data_" + name for name in [
     "determ",
-    "phase",
-    "amplitude",
+    # "phase",
+    # "amplitude",
     "phase_amplitude"
 ]
 ]
@@ -17,7 +18,11 @@ excel_files = ["data/data_" + name for name in [
 pogdon_data = ['kirill', 'alex', 'ilya']
 # Значения СКО по листам
 sigma = [0.25, 0.5, 1.0, 1.5, 0.01]
+signal_length = 64
+signal_amp = 2
 
+
+Laplace = lambda y: 1/2*(erf(y/np.sqrt(2)) + 1)
 
 def podgonian(P1, P2, sigma, std=0.02):
     ind1 = np.where(P1 > 1 - std)[0]
@@ -33,6 +38,7 @@ def podgonian(P1, P2, sigma, std=0.02):
 
 
 threshold = [0.5, 1, 3, 5, 7, 10, 15, 20, 23, 25, 27, 30]
+thr = np.logspace(np.log10(1e-9), np.log10(1000), 300)
 
 for j, excel_file in enumerate(excel_files):
     fig, ax = plt.subplots(figsize=(5, 5))
@@ -42,20 +48,36 @@ for j, excel_file in enumerate(excel_files):
         P2 = f.iloc[:, 3]
         # Правильное обнаружение
         P1 = f.iloc[:, 2]
-        sko = f.iloc[:, 1]
+        snr = f.iloc[:, 1]
+        snr_mean = np.mean(snr)
+        snr_nat = 10**(snr_mean/10)
+        print("Средний С/Ш:", snr_mean, 'дБ')
+        print("Средний С/Ш:", snr_nat)
 
-        P1, P2, sko = podgonian(P1, P2, sko)
+        # P1, P2, snr = podgonian(P1, P2, snr)
 
-        ax.plot(P2, P1, ".-", label="$\\text{СКО} = %.2f$" % sigma[i])
+        # Теория
+        N_0 = sigma[i]**2                             # СПМ шума
+        E_s = 0.5 * signal_amp**2 * signal_length     # Энергия сигнала
+        # d = np.sqrt(2 * E_s/N_0)
+        d = np.sqrt(4 * snr_nat)
+        P2_t = 1 - Laplace(np.log(thr)/d + d/2)       # Теория ЛТ
+        P1_t = 1 - Laplace(np.log(thr)/d - d/2)       # Теория ПО
+        
+        print(P2_t, P1_t, d)
+        ax.plot(P2_t, P1_t, "--", label="СКО = %.2f. Теория" % sigma[i])
+        ax.plot(P2, P1, ".-", label="СКО = %.2f" % sigma[i])
+        # ax.plot(P2_t, P1_t, label="$\\text{СКО} = %.2f$. Теория" % sigma[i])
+        # ax.plot(P2, P1, ".-", label="$\\text{СКО} = %.2f$" % sigma[i])
 
 
-        ax.plot(P2[2], P1[2], "o", color="darkblue", zorder=3)
-        ax.plot(P2[5], P1[5], "s", color="darkblue", zorder=3)
-        ax.plot(P2[7], P1[7], "^", color="darkblue", zorder=3)
+        # ax.plot(P2[2], P1[2], "o", color="darkblue", zorder=3)
+        # ax.plot(P2[5], P1[5], "s", color="darkblue", zorder=3)
+        # ax.plot(P2[7], P1[7], "^", color="darkblue", zorder=3)
 
         ax.set_title(labels[j])
-        ax.set_ylabel("$P_{\\text{ПО}}$")
-        ax.set_xlabel("$P_{\\text{ЛТ}}$")
+        # ax.set_ylabel("$P_{\\text{ПО}}$")
+        # ax.set_xlabel("$P_{\\text{ЛТ}}$")
         ax.grid(which='major', linestyle='-')
         ax.grid(which='minor', linestyle=':', alpha=0.5)
         ax.minorticks_on()
